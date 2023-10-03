7c12fa9acb0ba67d3fd80aeb5936e4a51cb4275d
nicol <nickibrt8@gmail.com>
2023-10-03 15:44:26 +0200

    Aggiunta documentazione a RnaStats.py

--- RnaStats.py
+++ RnaStats.py
@@ -6,37 +6,60 @@ from IncidenceProducer import IncidenceProducer
 
 
 class RnaStats:
-    def __init__(self, forna_file_path):
+    """Classe che raccoglie delle statistiche su una sequenza di RNA utilizzando un ipergrafo"""
+
+    def __init__(self, forna_file_path: str) -> None:
         incidence_dict = IncidenceProducer(forna_file_path).get_incidence_dict()
         self.H = hnx.Hypergraph(incidence_dict)
         del incidence_dict
-        self.__partitions = None
-        self.__precomputed_H = None
+        self.__partitions: list = None
+        self.__precomputed_H: list[set] = None
 
-    def plot_hypergraph(self, size=(40, 40)):
+    def plot_hypergraph(self, size: tuple = (40, 40)) -> None:
+        """Disegna un grafico che rappresenta l'ipergrafo costruito"""
         plt.subplots(figsize=size)
         hnx.draw(self.H)
         plt.show()
 
-    def partitions(self):
+    def partitions(self) -> list:
+        """Computa delle partizioni dell'ipergrafo"""
         if self.__precomputed_H is None:
             self.__precomputed_H = hmod.precompute_attributes(self.H)
         if self.__partitions is None:
             self.__partitions = hmod.kumar(self.__precomputed_H)
         return self.__partitions
 
-    def partition(self, n):
+    def partition(self, n: int) -> set:
+        """
+        Restituisce la partizione scelta
+        :param n: numero della partizione
+        :return: la partizione scelta
+        """
         self.partitions()
         return self.__partitions[n]
 
-    def modularity(self):
+    def modularity(self) -> float:
+        """
+        Restituisce la modularità dell'ipergrafo
+        :return: la modularità dell'ipergrafo
+        """
         self.partitions()
         return hmod.modularity(self.__precomputed_H, self.__partitions)
 
-    def __get_hyperedges(self, node):
+    def __get_hyperedges(self, node: int | str) -> list[int | str]:
+        """
+        Restituisce gli iperarchi che contengono il nodo selezionato
+        :param node: il nodo contenuto dagli iperarchi
+        :return: la lista di iperarchi che contengono il nodo selezionato
+        """
         return [edge for edge in self.H.edges if node in self.H.edges[edge]]
 
-    def get_subset_conductance(self, subset):
+    def get_subset_conductance(self, subset: set) -> float:
+        """
+        Restituisce la conduttanza di una partizione
+        :param subset: la partizione
+        :return: la conduttanza della partizione
+        """
         subset2 = set(self.H.nodes) - subset
         ws = sum([len(self.__get_hyperedges(node)) for node in subset])
         was = 0
@@ -49,12 +72,17 @@ class RnaStats:
             was += len(he_vertices)
         return was / ws
 
-    def get_partitions_conductance(self):
+    def get_partitions_conductance(self) -> enumerate[float]:
+        """
+        Restituisce la conduttanza di tutte le partizioni
+        :return: l'enumerazione contenente la conduttanza di tutte le partizioni
+        """
         return enumerate(
             [self.get_subset_conductance(subset) for subset in self.partitions()]
         )
 
-    def plot_partitions_conductance(self):
+    def plot_partitions_conductance(self) -> None:
+        """Disegna un grafico che rappresenta la conduttanza delle partizioni"""
         cond = self.get_partitions_conductance()
         seq = []
         values = []
@@ -67,10 +95,19 @@ class RnaStats:
         plt.ylabel("Conductance")
         plt.show()
 
-    def get_n_between_centrality(self, n=1):
+    def get_n_between_centrality(self, n: int = 1) -> dict:
+        """
+        Restituisce la n-between-centrality dei nucleotidi
+        :param n: connectedness requirement
+        :return: la n-between-centrality dei nucleotidi
+        """
         return hnx.algorithms.s_betweenness_centrality(self.H, n)
 
-    def plot_n_between_centrality(self, n=1):
+    def plot_n_between_centrality(self, n: int = 1) -> None:
+        """
+        Disegna un grafico che rappresenta la n-between-centrality dei nucleotidi
+        :param n: connectedness requirement
+        """
         centrality = self.get_n_between_centrality(n)
         seq = list(centrality.keys())
         centr = list(centrality.values())
