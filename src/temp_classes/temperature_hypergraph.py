5cf642608922882fcb4ba9a69d1a36535329179f
nicol <nickibrt8@gmail.com>
2023-11-11 21:35:07 +0100

    implementato ipergrafo  che non memorizza i folding identici a temperature diverse

--- src/temp_classes/temperature_hypergraph.py
+++ src/temp_classes/temperature_hypergraph.py
@@ -17,11 +17,30 @@ class TemporalHypergraph:
         pass
 
 
+class MemoryOptimizedFoldingHypergraph(TemporalHypergraph):
+
+    def __init__(self):
+        super().__init__()
+
+    def add_incidence_dict(self, incidence_dict, time):
+        found = False
+        for k, v in self.time_hypergraphs.items():
+            if incidence_dict == v.incidence_dict:
+                s = frozenset(sorted(k.union([time])))
+                self.time_hypergraphs[s] = self.time_hypergraphs[k]
+                del self.time_hypergraphs[k]
+                found = True
+                break
+        if not found:
+            self.time_hypergraphs[frozenset([time])] = hnx.Hypergraph(incidence_dict)
+        pass
+
+
 class TemperatureFoldingHypergraph:
 
     def __init__(self, producer: TemperatureIncidenceProducer) -> None:
         self.__producer = producer
-        self.__temperature_HG = TemporalHypergraph()
+        self.__temperature_HG = MemoryOptimizedFoldingHypergraph()
         self.__analyzed_temperatures = set()
 
     def insert_temperature(self, temperature):
