cc8e2be9b7058844f3904e4e6e13f30e9974815e
uninicol <nicol.buratti@studenti.unicam.it>
2023-12-08 19:21:52 +0100

    update main

--- src/main.py
+++ src/main.py
@@ -1,89 +1,42 @@
 import time
 import warnings
-from concurrent.futures import Executor, ThreadPoolExecutor, ProcessPoolExecutor
-from multiprocessing import Pool
+from pathlib import Path
 
 from Bio import SeqIO
 
-from src.temp_classes.rna_folder import RNAFolder
+from hypergraph_folding.rna_folder import RNAFolder
+from hypergraph_folding.temperature_hypergraph import TemperatureFoldingHypergraph, MemoryOptimizedFoldingHypergraph
+from incidence_producers.vienna_incidence_producer import ViennaIncidenceProducer
+from rna_stats.forna_rna_stats import TemperatureFoldingStats, RnaStats
 
 
 def timefunc(function, *args):
     start_time = time.perf_counter()
-    function(*args)
-    print(f"--- {time.perf_counter() - start_time:.2f} seconds ---")
-
-
-def temperature_dotbracket(parallel=False):
-    bracket_set = set()
-    if parallel:
-        with Pool() as pool:
-            for result in pool.imap(
-                    folder.temperature_folding, range(start_temp, end_temp + 1)
-            ):
-                bracket_set.add(result)
-    else:
-        for temperature in range(start_temp, end_temp + 1):
-            bracket_set.add(folder.temperature_folding(temperature))
-    print(f"numero di folding rilevati:\t{len(bracket_set)}")
-
-
-def temperature_dotbracket_exec(executor: Executor):
-    bracket_set = set()
-    with executor as exc:
-        for result in exc.map(
-                folder.temperature_folding, range(start_temp, end_temp + 1)
-        ):
-            bracket_set.add(result)
-    print(f"numero di folding rilevati:\t{len(bracket_set)}")
+    result = function(*args)
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
+    print(f"--- {total_time:.2f} seconds ---")
+    return total_time, result
 
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")  # non vengono scritti i warning di pandas
-
-    fasta_file = "../examples/sequence (7).fasta"
+    fasta_file = Path("../examples/sequence (3).fasta")
     sequence = str(next(SeqIO.parse(fasta_file, "fasta")).seq)
-    # thg = TemperatureHypergraph(ViennaIncidenceProducer(RNAFolder(sequence)))
-    # thg.insert_temperature(5)
-    # thg.insert_temperature(100)
-    # fasta_file = "examples/sequence (2).fasta"
-    start_temp = 0
-    end_temp = 100
-    start_temp = max(start_temp, -273)
-    # sequence = str(next(SeqIO.parse(fasta_file, "fasta")).seq)
-    folder = RNAFolder(sequence)
+    # sequence = "CCACGCACCUAAUCGAUAUAACUGCUUAUGG"
+    print(f"{len(sequence)=}")
 
-    print(f"{start_temp=}, {end_temp=}")
-    print(f"lunghezza sequenza:\t{len(sequence)}")
-    # print("parallelo:")
-    # timefunc(temperature_dotbracket, True)
-    # print("sequenziale:")
-    # timefunc(temperature_dotbracket, False)
-    print("Thread:")
-    timefunc(temperature_dotbracket, ThreadPoolExecutor())
-    print("Process:")
-    timefunc(temperature_dotbracket, ProcessPoolExecutor())
-    # for producer in [
-    #     # FornaIncidenceProducer(forna_file),
-    #     # ForgiIncidenceProducer(forna_file),
-    #     ViennaIncidenceProducer(RNAFolder(sequence)),
-    # ]:
-    #     rna_stats = FornaRnaStats(producer, 0)
-    #     rna_stats.plot_hypergraph()
-    #     rna_stats = FornaRnaStats(producer, 100)
-    #     rna_stats.plot_hypergraph()
-    #     print(rna_stats.secondary_structures())
-    #     print(f"modularity={rna_stats.modularity()}")
-    #     print("-" * 70)
-    # #     #
-    #     rna_stats.plot_partitions_conductance()
-    # #     # # for partition, conductance in enumerate(rna_stats.partitions_conductance()):
-    # #     # #     print(f"{partition=}, {conductance=}")
-    # #     # # print("-" * 70)
-    # #     # # for edge, centrality in rna_stats.n_between_centrality(n=1).items():
-    # #     # #     print(f"{edge=}, {centrality=}")
-    #     rna_stats.plot_n_between_centrality(n=1)
-    #     print(rna_stats.partitions())
-    #     print(rna_stats.secondary_structures())
-    #     # rna_stats.plot_n_between_centrality(n=2)
+    thg = TemperatureFoldingHypergraph(ViennaIncidenceProducer(RNAFolder(sequence)), MemoryOptimizedFoldingHypergraph())
+    thg.insert_temperature_range(80, 100)
+    stats = RnaStats(thg.get_hypergraph(80))
+    t_stats = TemperatureFoldingStats(thg)
+
+    stats.s_between_centrality()
+    stats.connection_differences(thg.get_hypergraph(100))
+    stats.get_nucleotides_change_structure(thg.get_hypergraph(100))
+    stats.partitions_conductance()
+    stats.structure_differences(thg.get_hypergraph(100))
+
+    t_stats.get_nucleotide_sensibility_to_changes(80, 100)
+    t_stats.get_structure_differences(80, 100)
 pass
